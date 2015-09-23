129087173d16b4adc1aeace08028fc49448bc9a7
Jason Wohlgemuth <jason.h.wohlgemuth@gmail.com>
2015-09-22 20:48:04 -0500

    Final push before starting work on techtonic-vm

--- bin/install_desktop.sh
+++ bin/install_desktop.sh
@@ -12,6 +12,7 @@ sudo apt-get install -y libxss1 libappindicator1 libindicator7 >/dev/null 2>&1
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb >/dev/null 2>&1
 sudo apt-get install -f >/dev/null 2>&1
 sudo dpkg -i google-chrome*.deb >/dev/null 2>&1
+sudo rm google-chrome-stable_current_amd64.deb
 
 printf "Start: "$START
 printf "End:   "$(date '+%T')
