70a767f5780c899be8b547d2e5fa00b5239d5dcd
Jason Wohlgemuth <jason.h.wohlgemuth@gmail.com>
2015-10-03 20:26:41 -0500

    Updated atom install from build to download and install

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -6,7 +6,7 @@ apt-get install -y default-jre default-jdk >/dev/null 2>&1
 # Install Node and npm
 # --------------------
 echo "Preparing to install node........"$(date '+%T')
-curl -sL https://deb.nodesource.com/setup | sudo bash - >/dev/null 2>&1
+curl -sL https://deb.nodesource.com/setup | bash - >/dev/null 2>&1
 echo "Installing node.js and npm......."$(date '+%T')
 apt-get install -y nodejs >/dev/null 2>&1
 echo "Installing global node modules..."$(date '+%T')
--- share/install_atom.sh
+++ share/install_atom.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
-#Atom editor build dependencies
-sudo apt-get install -y libgnome-keyring-dev >/dev/null 2>&1
 # Install Atom Editor and plugins
 # -------------------------------
 echo "Installing Atom editor..........."$(date '+%T')
-mkdir ~/git
-cd ~/git
-git clone https://github.com/atom/atom
-cd ~/git/atom
-git fetch -p >/dev/null 2>&1
-git checkout $(git describe --tags `git rev-list --tags --max-count=1`) >/dev/null 2>&1
-script/build
-sudo script/grunt install
+apt-get update
+apt-get install -y python-pip
+pip install ipython[notebook]
+wget https://github.com/atom/atom/releases/download/v1.1.0-beta.0/atom-amd64.deb
+sudo dpkg -i atom-amd64.deb
+sudo rm atom-amd64.deb
+
 echo "Installing Atom plugins..."
-apm install minimap file-icons imdone-atom
+apm install minimap file-icons atom-beautify imdone-atom
