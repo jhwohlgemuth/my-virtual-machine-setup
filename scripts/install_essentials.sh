136c95de0e16703303c17f53b715d47fd3d11373
Jason Wohlgemuth <jason.h.wohlgemuth@gmail.com>
2015-10-04 21:16:02 -0500

    More changes

--- scripts/install_essentials.sh
+++ scripts/install_essentials.sh
@@ -6,12 +6,21 @@ echo "Installing JRE and JDK..........."$(date '+%T')
 apt-get install -y default-jre default-jdk >/dev/null 2>&1
 # Install Node and npm
 # --------------------
-echo "Preparing to install node........"$(date '+%T')
-curl -sL https://deb.nodesource.com/setup | bash - >/dev/null 2>&1
 echo "Installing node.js and npm......."$(date '+%T')
-apt-get install -y nodejs >/dev/null 2>&1
+apt-get install -y nodejs npm >/dev/null 2>&1
+ln -s /usr/bin/nodejs /usr/bin/node
 echo "Installing global node modules..."$(date '+%T')
 npm install phantomjs casperjs yo bower grunt-cli -g >/dev/null 2>&1
+# Install Atom Editor and plugins
+# -------------------------------
+echo "Installing Atom editor..........."$(date '+%T')
+apt-get install -y libzmq3-dev python-pip
+pip install ipython[notebook]
+wget https://github.com/atom/atom/releases/download/v1.1.0-beta.0/atom-amd64.deb
+dpkg -i atom-amd64.deb
+rm atom-amd64.deb
+echo "Installing Atom plugins.........."$(date '+%T')
+apm install minimap file-icons atom-beautify imdone-atom hydrogen
 # Install Pandoc
 # --------------
 apt-get install -y texlive texlive-latex-extra pandoc
--- share/install_atom.sh
+++ share/install_atom.sh
@@ -4,7 +4,7 @@
 echo "Installing Atom editor..........."$(date '+%T')
 sudo apt-get update
 sudo apt-get install -y libzmq3-dev python-pip
-pip install ipython[notebook]
+sudo pip install ipython[notebook]
 wget https://github.com/atom/atom/releases/download/v1.1.0-beta.0/atom-amd64.deb
 sudo dpkg -i atom-amd64.deb
 sudo rm atom-amd64.deb
