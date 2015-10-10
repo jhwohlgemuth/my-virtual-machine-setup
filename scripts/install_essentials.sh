0abd910c456f92b829cd0ecce433c55489c89d69
Jason Wohlgemuth <jason.h.wohlgemuth@gmail.com>
2015-10-09 20:37:36 -0500

    working on stuff...WIP

--- scripts/install_essentials.sh
+++ scripts/install_essentials.sh
@@ -10,18 +10,24 @@ echo "Installing JRE and JDK..........."$(date '+%T')
 #apt-get update >/dev/null 2>&1
 #apt-get install -y oracle-java8-installer >/dev/null 2>&1
 
-echo "Installing nvm, node & npm......."$(date '+%T')
+echo "Installing nvm..................."$(date '+%T')
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 
 echo "Installing Pandoc................"$(date '+%T')
 #apt-get install -y texlive texlive-latex-extra pandoc >/dev/null 2>&1
 
+echo "Installing Atom editor..........."$(date '+%T')
+sudo add-apt-repository -y ppa:webupd8team/atom
+sudo apt-get update >/dev/null 2>&1
+sudo apt-get install -y atom >/dev/null 2>&1
+
 echo "Installing miscellaneous items..."$(date '+%T')
 #add-apt-repository -y ppa:avsm/ppa
 #apt-get update >/dev/null 2>&1
 #apt-get install -y ocaml ocaml-native-compilers camlp4-extra opam >/dev/null 2>&1
-apt-get install -y libzmq3-dev python-pip python-dev >/dev/null 2>&1
-pip install --upgrade pip
-pip install --upgrade virtualenv
-pip install ipython[notebook] >/dev/null 2>&1
-apt-get install -y figlet toilet >/dev/null 2>&1
+
+#apt-get install -y libzmq3-dev python-pip python-dev >/dev/null 2>&1
+#pip install --upgrade pip
+#pip install --upgrade virtualenv
+#pip install ipython[notebook] >/dev/null 2>&1
+#apt-get install -y figlet toilet >/dev/null 2>&1
--- share/install_node.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-sudo apt-get update
-# Install Node and npm
-# --------------------
-echo "Installing node.js and npm......."$(date '+%T')
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
