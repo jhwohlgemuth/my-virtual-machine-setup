ecc81f49c265b378885f6482e06a4d2eb4046676
Jason Wohlgemuth <jason.h.wohlgemuth@gmail.com>
2015-10-11 18:40:49 -0500

    Refactored install files...moved python stuff into install_software...WIP

---
+++ share/install_julia.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+apt-get update >/dev/null 2>&1
+
+echo "Adding Julia language PPA........"
+apt-get install -y software-properties-common python-software-properties >/dev/null 2>&1
+add-apt-repository -y ppa:staticfloat/juliareleases >/dev/null 2>&1
+add-apt-repository -y ppa:staticfloat/julia-deps >/dev/null 2>&1
+apt-get update >/dev/null 2>&1
+echo "Installing Julia language........"
+apt-get install -y julia >/dev/null 2>&1
+
+#julia Pkg.add("IJulia")
