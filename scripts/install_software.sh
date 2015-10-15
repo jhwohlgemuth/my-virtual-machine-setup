5a150e7cd8133f7dbf6984fb9d340b54586b4d0a
Jason Wohlgemuth <jason.h.wohlgemuth@gmail.com>
2015-10-15 12:34:21 -0500

    Seems functional...working back to complete...WIP

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -10,7 +10,7 @@ apt-get update >/dev/null 2>&1
 apt-get install -y oracle-java8-installer >/dev/null 2>&1
 
 echo "Installing Pandoc................"$(date '+%T')
-apt-get install -y texlive texlive-latex-extra pandoc >/dev/null 2>&1
+#apt-get install -y texlive texlive-latex-extra pandoc >/dev/null 2>&1
 
 echo "Installing Atom editor..........."$(date '+%T')
 add-apt-repository -y ppa:webupd8team/atom >/dev/null 2>&1
@@ -19,19 +19,19 @@ apt-get install -y atom >/dev/null 2>&1
 
 echo "Installing Python dependencies..."$(date '+%T')
 apt-get install -y libzmq3-dev python-pip python-dev >/dev/null 2>&1
-apt-get install -y libblas-dev libatlas-base-dev liblapack-dev gfortran libfreetype6-dev libpng-dev >/dev/null 2>&1
+#apt-get install -y libblas-dev libatlas-base-dev liblapack-dev gfortran libfreetype6-dev libpng-dev >/dev/null 2>&1
 pip install --upgrade pip
 pip install --upgrade virtualenv
 pip install ipython[notebook] >/dev/null 2>&1
 
 echo "Adding Julia language PPA........"$(date '+%T')
-apt-get install -y software-properties-common python-software-properties >/dev/null 2>&1
-add-apt-repository -y ppa:staticfloat/juliareleases >/dev/null 2>&1
-add-apt-repository -y ppa:staticfloat/julia-deps >/dev/null 2>&1
-apt-get update >/dev/null 2>&1
+#apt-get install -y software-properties-common python-software-properties >/dev/null 2>&1
+#add-apt-repository -y ppa:staticfloat/juliareleases >/dev/null 2>&1
+#add-apt-repository -y ppa:staticfloat/julia-deps >/dev/null 2>&1
+#apt-get update >/dev/null 2>&1
 echo "Installing Julia language........"$(date '+%T')
-apt-get install -y julia >/dev/null 2>&1
-julia -e 'Pkg.add("IJulia")' >/dev/null 2>&1
+#apt-get install -y julia >/dev/null 2>&1
+#julia -e 'Pkg.add("IJulia")' >/dev/null 2>&1
 
 echo "Installing miscellaneous........."$(date '+%T')
 apt-get install -y figlet toilet >/dev/null 2>&1
