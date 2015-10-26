2ba6d11747f17d3fe78ab6384568d0d99a1b9641
Jason Wohlgemuth <jason.h.wohlgemuth@gmail.com>
2015-10-26 15:23:24 -0500

    Removed comment from install_jenkins

--- share/functions.sh
+++ share/functions.sh
@@ -50,12 +50,6 @@ install_java8() {
 }
 
 install_jenkins() {
-    # Jenkins will be launched as a daemon up on start.
-    # See /etc/init.d/jenkins for more details.
-    # The 'jenkins' user is created to run this service.
-    # Log file will be placed in /var/log/jenkins/jenkins.log.
-    # Check this file if you are troubleshooting Jenkins.
-    # /etc/default/jenkins will capture configuration parameters for the launch like e.g JENKINS_HOME
     log "Preparing to install Jenkins"
     wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add - >/dev/null 2>&1
     sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list' >/dev/null 2>&1
