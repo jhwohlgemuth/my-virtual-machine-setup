4e18d318ec6f6857950d54abfeda87f9ed3283f3
Jason Wohlgemuth <jason.h.wohlgemuth@gmail.com>
2015-10-27 19:18:13 -0500

    Removed nuclide and added port deconfliction

--- scripts/install_software_no_sudo.sh
+++ scripts/install_software_no_sudo.sh
@@ -8,7 +8,7 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | b
 log "Installing Atom plugins"
 apm install file-icons sublime-block-comment atom-beautify >/dev/null 2>&1
 apm install minimap minimap-codeglance minimap-selection minimap-find-and-replace minimap-git-diff >/dev/null 2>&1
-apm install hydrogen nuclide-installer >/dev/null 2>&1
+apm install hydrogen >/dev/null 2>&1
 
 if type julia >/dev/null 2>&1; then
     log "Installing IJulia"
