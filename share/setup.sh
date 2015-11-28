61c17bf25302ea2b9034ac742a2e662754d9c85c
Jason Wohlgemuth <jason.h.wohlgemuth@gmail.com>
2015-11-28 09:28:34 -0600

    Minor tweaks before new build

--- share/setup.sh
+++ share/setup.sh
@@ -10,10 +10,10 @@ echo "Setting terminal theme............"$(TZ=":US/$TIMEZONE" date +%T)
 sed -i '/ZSH_THEME/c ZSH_THEME="agnoster"' ~/.zshrc
 sed -i '/plugins=(/c plugins=(git git-extras npm docker encode64 jsontools web-search wd)' ~/.zshrc
 echo "export NVM_DIR=/home/${SSH_USER}/.nvm" >> ~/.zshrc
+echo "[ -s '$NVM_DIR/nvm.sh' ] && . '$NVM_DIR/nvm.sh'" >> ~/.zshrc
 echo "dip() { docker inspect --format '{{ .NetworkSettings.IPAddress }}' \$1 ; }" >> ~/.zshrc
 echo "docker_rm_all() { docker stop \$(docker ps -a -q) && docker rm \$(docker ps -a -q) ; }" >> ~/.zshrc
 echo "source /home/${SSH_USER}/.${ORG_NAME}/functions.sh" >> ~/.zshrc
-echo "[ -s '$NVM_DIR/nvm.sh' ] && . '$NVM_DIR/nvm.sh'" >> ~/.zshrc
 echo $SSH_PASSWORD | sudo -S chsh -s $(which zsh) $(whoami)
 
 echo "Turning on workspaces (unity)....."$(TZ=":US/$TIMEZONE" date +%T)
