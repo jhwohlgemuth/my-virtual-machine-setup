30856296ada2145813d3f526e0901b4fe028077d
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-09-04 15:25:49 -0500

    Re-removed julia from installed software

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -6,10 +6,10 @@ update
 install_desktop
 install_java8
 install_python
-install_julia
 install_atom
 install_docker
 # install_jenkins
+# install_julia
 # install_redis
 # install_couchdb
 # install_mongodb
