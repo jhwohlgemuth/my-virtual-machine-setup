d7ae182b92bbc1c1cadadfbd103136bafa23cc43
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2018-03-16 17:09:27 -0500

    Add packages to xenial install_desktop

--- scripts/xenial/install_desktop.sh
+++ scripts/xenial/install_desktop.sh
@@ -10,6 +10,8 @@ log "Checking version of Ubuntu"
 log "Installing ubuntu-desktop"
 apt-get install -y --no-install-recommends ubuntu-desktop >/dev/null 2>&1
 apt-get install -y gnome-terminal overlay-scrollbar gnome-session-flashback >/dev/null 2>&1
+apt-get install -y firefox chromium-browser ubuntu-restricted-addons htop indicator-multiload xclip tree >/dev/null 2>&1
+apt-get install -y figlet toilet >/dev/null 2>&1
 
 LIGHTDM_CONFIG=/etc/lightdm/lightdm.conf
 GDM_CUSTOM_CONFIG=/etc/gdm/custom.conf
