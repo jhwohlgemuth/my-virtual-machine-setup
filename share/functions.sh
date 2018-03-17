0b407eaafb9349c3adbfd5657d7aa613bd090f8e
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2018-03-17 13:56:43 -0500

    Un-silence Atom plugin installations

--- share/functions.sh
+++ share/functions.sh
@@ -239,12 +239,11 @@ install_popular_atom_plugins() {
     if type apm >/dev/null 2>&1; then
         log "Installing Atom plugins"
         #editor and language plugins
-        apm install file-icons sublime-block-comment atom-beautify language-babel >/dev/null 2>&1
-        apm install emmet atom-alignment atom-ternjs atom-terminal color-picker pigments atom-quokka >/dev/null 2>&1
+        apm install file-icons sublime-block-comment atom-beautify language-babel emmet atom-alignment atom-ternjs atom-terminal color-picker pigments atom-quokka
         #minimap plugins
-        apm install minimap minimap-selection minimap-find-and-replace minimap-git-diff >/dev/null 2>&1
+        apm install minimap minimap-selection minimap-find-and-replace minimap-git-diff
         #svg plugins
-        apm install language-svg svg-preview >/dev/null 2>&1
+        apm install language-svg svg-preview
     else
         log "Please install Atom before installing Atom plugins"
     fi
