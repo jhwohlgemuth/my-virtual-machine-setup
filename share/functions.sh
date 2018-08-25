31d7be19f12f0c6179129013e372760ab794a67f
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2018-08-25 18:34:26 +0300

    Fix issues with install_mono

--- share/functions.sh
+++ share/functions.sh
@@ -243,9 +243,7 @@ install_mono() {
     run apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
     # echo "deb http://download.mono-project.com/repo/ubuntu stable-trusty main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list >/dev/null 2>&1
     echo "deb https://download.mono-project.com/repo/ubuntu stable-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list >/dev/null 2>&1
-    sudo apt install apt-transport-https
-    sudo apt update
-
+    run apt-get install apt-transport-https --yes
     update
     log "Installing mono"
     run apt-get install mono-devel -y --force-yes
