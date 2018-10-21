4b3634911cdfbfb7b8f27f189382c03ddb107f2d
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2018-10-21 15:44:16 +0300

    Clear ShellCheck issues in install_provider_extras.sh

--- scripts/common/install_provider_extras.sh
+++ scripts/common/install_provider_extras.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC1091
 . ./functions.sh
 
 SSH_USER=${SSH_USERNAME:-vagrant}
 
-if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
+if [[ "$PACKER_BUILDER_TYPE" =~ virtualbox ]]; then
     log "Installing Virtualbox guest additions"
-    VBOX_VERSION=$(cat /home/${SSH_USER}/.vbox_version)
-    mount -o loop /home/${SSH_USER}/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt >/dev/null 2>&1
+    VBOX_VERSION=$(cat /home/"${SSH_USER}"/.vbox_version)
+    mount -o loop /home/"${SSH_USER}"/VBoxGuestAdditions_"$VBOX_VERSION".iso /mnt >/dev/null 2>&1
     sh /mnt/VBoxLinuxAdditions.run >/dev/null 2>&1
     umount /mnt >/dev/null 2>&1
-    rm /home/${SSH_USER}/VBoxGuestAdditions_$VBOX_VERSION.iso
-    rm /home/${SSH_USER}/.vbox_version
-    if [[ $VBOX_VERSION = "4.3.10" ]]; then
+    rm /home/"${SSH_USER}"/VBoxGuestAdditions_"$VBOX_VERSION".iso
+    rm /home/"${SSH_USER}"/.vbox_version
+    if [[ "$VBOX_VERSION" = "4.3.10" ]]; then
         ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
     fi
 fi
-if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
+if [[ "$PACKER_BUILDER_TYPE" =~ vmware ]]; then
     function install_open_vm_tools {
         log "Installing Open VM Tools"
         apt-get install -y open-vm-tools >/dev/null 2>&1
@@ -25,47 +26,47 @@ if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
     }
     function install_vmware_tools {
         log "Installing VMware Tools"
-        cd /tmp
+        cd /tmp || return
         mkdir -p /mnt/cdrom
-        mount -o loop /home/${SSH_USERNAME}/linux.iso /mnt/cdrom
+        mount -o loop /home/"${SSH_USERNAME}"/linux.iso /mnt/cdrom
         VMWARE_TOOLS_PATH=$(ls /mnt/cdrom/VMwareTools-*.tar.gz)
         VMWARE_TOOLS_VERSION=$(echo "${VMWARE_TOOLS_PATH}" | cut -f2 -d'-')
         VMWARE_TOOLS_BUILD=$(echo "${VMWARE_TOOLS_PATH}" | cut -f3 -d'-')
-        VMWARE_TOOLS_BUILD=$(basename ${VMWARE_TOOLS_BUILD} .tar.gz)
+        VMWARE_TOOLS_BUILD=$(basename "${VMWARE_TOOLS_BUILD}" .tar.gz)
         echo "==> VMware Tools Path: ${VMWARE_TOOLS_PATH}"
         echo "==> VMWare Tools Version: ${VMWARE_TOOLS_VERSION}"
         echo "==> VMware Tools Build: ${VMWARE_TOOLS_BUILD}"
         tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
-        VMWARE_TOOLS_MAJOR_VERSION=$(echo ${VMWARE_TOOLS_VERSION} | cut -d '.' -f 1)
+        VMWARE_TOOLS_MAJOR_VERSION=$(echo "${VMWARE_TOOLS_VERSION}" | cut -d '.' -f 1)
         if [ "${VMWARE_TOOLS_MAJOR_VERSION}" -lt "10" ]; then
             /tmp/vmware-tools-distrib/vmware-install.pl -d
         else
             /tmp/vmware-tools-distrib/vmware-install.pl -f
         fi
-        rm /home/${SSH_USERNAME}/linux.iso
+        rm /home/"${SSH_USERNAME}"/linux.iso
         umount /mnt/cdrom
         rmdir /mnt/cdrom
         rm -rf /tmp/VMwareTools-*
         VMWARE_TOOLBOX_CMD_VERSION=$(vmware-toolbox-cmd -v)
         log "Installed VMware Tools ${VMWARE_TOOLBOX_CMD_VERSION}"
     }
-    if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
+    if [[ "$PACKER_BUILDER_TYPE" =~ vmware ]]; then
         KERNEL_VERSION=$(uname -r | cut -d. -f1-2)
         echo "==> Kernel version ${KERNEL_VERSION}"
-        MAJOR_VERSION=$(echo ${KERNEL_VERSION} | cut -d '.' -f1)
-        MINOR_VERSION=$(echo ${KERNEL_VERSION} | cut -d '.' -f2)
+        MAJOR_VERSION=$(echo "${KERNEL_VERSION}" | cut -d '.' -f1)
+        MINOR_VERSION=$(echo "${KERNEL_VERSION}" | cut -d '.' -f2)
         if [ "${MAJOR_VERSION}" -ge "4" ] && [ "${MINOR_VERSION}" -ge "1" ]; then
             # open-vm-tools supports shared folders on kernel 4.1 or greater
             . /etc/lsb-release
-            if [[ $DISTRIB_RELEASE == 14.04 ]]; then
+            if [[ "$DISTRIB_RELEASE" == 14.04 ]]; then
                 install_vmware_tools
                 # Ensure that VMWare Tools recompiles kernel modules
                 echo "answer AUTO_KMODS_ENABLED yes" >> /etc/vmware-tools/locations
-            else 
+            else
                 install_open_vm_tools
             fi
         else
             install_vmware_tools
-        fi 
+        fi
     fi
 fi
