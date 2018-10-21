934380a8b61dd74c94b201d99f882d66bbd7f6c8
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2018-10-21 14:47:53 +0300

    Clear ShellCheck issues in setup.sh

--- share/setup.sh
+++ share/setup.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-if [ `whoami` == 'root' ]; then
+if [[ $(whoami) == "root" ]]; then
     echo "✘ Setup should not be run as root"
     return 0
 fi
 
 ORG_NAME=${ORG_NAME:-jhwohlgemuth}
 
-#Source log function
-. ${HOME}/.${ORG_NAME}/functions.sh
+# Source log function
+# shellcheck disable=SC1090
+. "${HOME}"/."${ORG_NAME}"/functions.sh
 
 turn_on_workspaces
 turn_off_screen_lock
@@ -16,12 +17,14 @@ install_ohmyzsh
 customize_ohmyzsh
 
 log "Installing node"
+# shellcheck disable=SC1090
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 nvm install node && nvm alias default node
 install_popular_node_modules
 
 log "Installing Ruby and ruby gems"
-. ${HOME}/.rvm/scripts/rvm
+# shellcheck disable=SC1090
+. "${HOME}"/.rvm/scripts/rvm
 rvm use --default --install 2.4
 
 if type toilet >/dev/null 2>&1; then
