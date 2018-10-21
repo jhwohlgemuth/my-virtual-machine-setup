4a9fde77fa9c85c7a5c25626b32fe3d9030f025b
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2018-10-21 15:20:10 +0300

    Clear ShellCheck issues in configure_sudo.sh

--- scripts/common/configure_sudo.sh
+++ scripts/common/configure_sudo.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC1091
 . ./functions.sh
 
 date > /etc/box_build_time
@@ -9,17 +10,17 @@ SSH_USER_HOME=${SSH_USER_HOME:-/home/${SSH_USER}}
 VAGRANT_INSECURE_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
 
 # Create Vagrant user (if not already present)
-if ! id -u $SSH_USER >/dev/null 2>&1; then
+if ! id -u "$SSH_USER" >/dev/null 2>&1; then
     log "Creating $SSH_USER user"
-    /usr/sbin/groupadd $SSH_USER
-    /usr/sbin/useradd $SSH_USER -g $SSH_USER -G sudo -d $SSH_USER_HOME --create-home
+    /usr/sbin/groupadd "$SSH_USER"
+    /usr/sbin/useradd "$SSH_USER" -g "$SSH_USER" -G sudo -d "$SSH_USER_HOME" --create-home
     echo "${SSH_USER}:${SSH_PASS}" | chpasswd
 fi
 
 # Set up sudo
 log "Giving ${SSH_USER} sudo powers"
-echo "${SSH_USER}        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/$SSH_USER
-chmod 440 /etc/sudoers.d/$SSH_USER
+echo "${SSH_USER}        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/"$SSH_USER"
+chmod 440 /etc/sudoers.d/"$SSH_USER"
 
 # Fix stdin not being a tty
 if grep -q -E "^mesg n$" /root/.profile && sed -i "s/^mesg n$/tty -s \\&\\& mesg n/g" /root/.profile; then
@@ -27,14 +28,14 @@ if grep -q -E "^mesg n$" /root/.profile && sed -i "s/^mesg n$/tty -s \\&\\& mesg
 fi
 
 log "Installing vagrant key"
-mkdir $SSH_USER_HOME/.ssh
-chmod 700 $SSH_USER_HOME/.ssh
-cd $SSH_USER_HOME/.ssh
+mkdir "$SSH_USER_HOME"/.ssh
+chmod 700 "$SSH_USER_HOME"/.ssh
+cd "$SSH_USER_HOME"/.ssh || exit
 
 # https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
-echo "${VAGRANT_INSECURE_KEY}" > $SSH_USER_HOME/.ssh/authorized_keys
-chmod 600 $SSH_USER_HOME/.ssh/authorized_keys
-chown -R $SSH_USER:$SSH_USER $SSH_USER_HOME/.ssh
+echo "${VAGRANT_INSECURE_KEY}" > "$SSH_USER_HOME"/.ssh/authorized_keys
+chmod 600 "$SSH_USER_HOME"/.ssh/authorized_keys
+chown -R "$SSH_USER":"$SSH_USER" "$SSH_USER_HOME"/.ssh
 
 # Security setting
 echo "UseDNS no" >> /etc/ssh/sshd_config
