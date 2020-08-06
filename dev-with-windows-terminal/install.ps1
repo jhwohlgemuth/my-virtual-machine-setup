1203b50da07def0582da3169593adc329bf550ac
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2020-08-06 13:40:15 -0500

    feat: Refine and augment install.ps1

--- dev-with-windows-terminal/install.ps1
+++ dev-with-windows-terminal/install.ps1
@@ -1,40 +1,61 @@
+$CHOCOLATEY_PACKAGES = @(
+    '7zip'
+    'bat'
+    'cascadiafonts'
+    'ccleaner'
+    'dropbox'
+    'firacode'
+    'firefox'
+    'git'
+    'googlechrome'
+    'googledrive'
+    'insomnia-rest-api-client'
+    'itunes'
+    'jq'
+    'lockhunter'
+    'malwarebytes'
+    'make'
+    'miktex'
+    'nordvpn'
+    'nvm'
+    'packer'
+    'pandoc'
+    'speccy'
+    'steam'
+    'sysinternals'
+    'teracopy'
+    'vscode'
+    'vagrant'
+    'virtualbox'
+    'windirstat'
+    'zotero'
+)
+$POWERSHELL_MODULES = @(
+    'posh-git'
+    'oh-my-posh'
+    'PSConsoleTheme'
+    'Get-ChildItemColor'
+)
+
+function Test-Installed
+{
+  $Name = $args[0]
+  Get-Module -ListAvailable -Name $Name
+}
+function Install-ModuleMaybe
+{
+    $Name = $args[0]
+    if (Test-Installed $Name) {
+        Write-Output "==> $Name already installed"
+    } else {
+        Write-Output "==> Installing $Name"
+        Install-Module -Name $Name -Scope CurrentUser -AllowClobber
+    }
+}
+
+# Install Chocolatey packages
 choco feature enable -n allowGlobalConfirmation
+$CHOCOLATEY_PACKAGES | ForEach-Object { choco install $_ }
 
-# Windows Software
-choco install 7zip
-choco install bat
-choco install cascadiafonts
-choco install ccleaner
-choco install dropbox
-choco install firacode
-choco install firefox
-choco install git
-choco install googlechrome
-choco install googledrive
-choco install insomnia-rest-api-client
-choco install itunes
-choco install jq
-choco install lockhunter
-choco install malwarebytes
-choco install make
-choco install miktex
-choco install nordvpn
-choco install nvm
-choco install packer
-choco install pandoc
-choco install speccy
-choco install steam
-choco install sysinternals
-choco install teracopy
-choco install vscode
-choco install vagrant
-choco install virtualbox
-choco install windirstat
-choco install zotero
-
-# Powershell modules
-Install-Module posh-git -Scope CurrentUser
-Install-Module oh-my-posh -Scope CurrentUser
-Install-Module -Name PSConsoleTheme
-Install-Module -Name Get-ChildItemColor -AllowClobber -Scope CurrentUser
-Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
+# Install Powershell modules
+$POWERSHELL_MODULES | Install-ModuleMaybe
