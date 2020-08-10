fe211d156998f777e20ae04491153eff91f756c4
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2020-08-10 14:05:03 -0500

    feat: Order PS functions alphabetically, add README instructions

--- dev-with-windows-terminal/Microsoft.Powershell_profile.ps1
+++ dev-with-windows-terminal/Microsoft.Powershell_profile.ps1
@@ -1,3 +1,112 @@
+function Find-Duplicates
+{
+  <#
+  .SYNOPSIS
+  Helper function that calculates file hash values to find duplicate files recursively
+  .EXAMPLE
+  Find-Duplicates <path to folder>
+  .EXAMPLE
+  pwd | Find-Duplicates
+  #>
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+    [string] $Name
+  )
+  Get-Item $Name | Get-ChildItem -Recurse | Get-FileHash | Group-Object -Property Hash | Where-Object Count -GT 1 | ForEach-Object {$_.Group | Select-Object Path, Hash} | Write-Output
+}
+function New-File
+{
+  <#
+  .SYNOPSIS
+  Powershell equivalent of linux "touch" command
+  .EXAMPLE
+  New-File <file name>
+  #>
+  [CmdletBinding(SupportsShouldProcess=$true)]
+  param (
+    [Parameter(Mandatory=$true)]
+    [string] $Name
+  )
+  if (Test-Path $Name) {
+    (Get-ChildItem $Name).LastWriteTime = Get-Date
+  } else {
+    New-Item -Path . -Name $Name -ItemType "file" -Value ""
+  }
+}
+function New-SshKey
+{
+  [CmdletBinding()]
+  param(
+    [Parameter()]
+    [string] $Name="id_rsa")
+  Write-Verbose "==> Generating SSH key pair"
+  $Path = "~/.ssh/$Name"
+  ssh-keygen --% -q -b 4096 -t rsa -N "" -f TEMPORARY_FILE_NAME
+  Move-Item -Path TEMPORARY_FILE_NAME -Destination $Path
+  Move-Item -Path TEMPORARY_FILE_NAME.pub -Destination "$Path.pub"
+  if (Test-Path "$Path.pub") {
+    Write-Verbose "==> $Name SSH private key saved to $Path"
+    Write-Verbose "==> Saving SSH public key to clipboard"
+    Get-Content "$Path.pub" | Set-Clipboard
+    Write-Output "==> Public key saved to clipboard"
+  } else {
+    Write-Error "==> Failed to create SSH key"
+  }
+}
+function Remove-DirectoryForce
+{
+  <#
+  .SYNOPSIS
+  Powershell equivalent of linux "rm -frd"
+  .EXAMPLE
+  rf <folder name>
+  #>
+  [CmdletBinding(SupportsShouldProcess=$true)]
+  param (
+    [Parameter(Mandatory=$true)]
+    [string] $Name
+  )
+  $Path = Join-Path (Get-Location) $Name
+  if (Test-Path $Path) {
+    $Cleaned = Resolve-Path $Path
+    Write-Verbose "=> Deleting $Cleaned"
+    Remove-Item -Path $Cleaned -Recurse
+    Write-Verbose "=> Deleted $Cleaned"
+  } else {
+    Write-Error 'Bad input. No folders/files were deleted'
+  }
+}
+function Take
+{
+  <#
+  .SYNOPSIS
+  Powershell equivalent of oh-my-zsh take function
+  .DESCRIPTION
+  Using take will create a new directory and then enter the driectory
+  .EXAMPLE
+  take <folder name>
+  #>
+  [CmdletBinding(SupportsShouldProcess=$true)]
+  param (
+    [Parameter(Mandatory=$true)]
+    [string] $Name
+  )
+  $Path = Join-Path (Get-Location) $Name
+  if (Test-Path $Path) {
+    Write-Verbose "=> $Path exists"
+    Write-Verbose "=> Entering $Path"
+    Set-Location $Path
+  } else {
+    Write-Verbose "=> Creating $Path"
+    mkdir $Path
+    if (Test-Path $Path) {
+      Write-Verbose "=> Entering $Path"
+      Set-Location $Path
+    }
+  }
+  Write-Verbose "=> pwd is $(Get-Location)"
+}
 function Test-Admin
 {
   <#
@@ -49,160 +158,6 @@ function Install-ModuleMaybe
       Install-Module -Name $Name -Scope CurrentUser -AllowClobber
   }
 }
-
-# Install Powershell modules, if not already installed
-if (Test-Installed posh-git) {
-  Import-Module posh-git
-}
-if (Test-Installed oh-my-posh) {
-  Import-Module oh-my-posh
-}
-if (Test-Installed Get-ChildItemColor) {
-  Import-Module Get-ChildItemColor
-}
-
-Set-Theme Agnoster
-
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path $ChocolateyProfile) {
-  Import-Module "$ChocolateyProfile"
-}
-
-# Helper functions
-# Create directory traversal functions, "u", "uu", etc...
-for($i = 1; $i -le 5; $i++) {
-  $u =  "".PadLeft($i,"u")
-  $d =  $u.Replace("u","../")
-  Invoke-Expression "function $u { push-location $d }"
-}
-
-function home { Set-Location ~ }
-function Invoke-DockerInspectAddress { docker inspect --format '{{ .NetworkSettings.IPAddress }}' $args[0] }
-function Invoke-DockerRemoveAll { docker stop $(docker ps -a -q); docker rm $(docker ps -a -q) }
-function Invoke-DockerRemoveAllImages { docker rmi $(docker images -a -q) }
-function Invoke-GitCommand { git $args }
-function Invoke-GitCommit { git commit -vam $args }
-function Invoke-GitDiff { git diff $args }
-function Invoke-GitPushMaster { git push origin master }
-function Invoke-GitStatus { git status -sb }
-function Invoke-GitRebase { git rebase -i $args }
-function Invoke-GitLog { git log --oneline --decorate }
-
-function Find-Duplicates
-{
-  <#
-  .SYNOPSIS
-  Helper function that calculates file hash values to find duplicate files recursively
-  .EXAMPLE
-  Find-Duplicates <path to folder>
-  .EXAMPLE
-  pwd | Find-Duplicates
-  #>
-  [CmdletBinding()]
-  param (
-    [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
-    [string] $Name
-  )
-  Get-Item $Name | Get-ChildItem -Recurse | Get-FileHash | Group-Object -Property Hash | Where-Object Count -GT 1 | ForEach-Object {$_.Group | Select-Object Path, Hash} | Write-Output
-}
-
-function Remove-DirectoryForce
-{
-  <#
-  .SYNOPSIS
-  Powershell equivalent of linux "rm -frd"
-  .EXAMPLE
-  rf <folder name>
-  #>
-  [CmdletBinding(SupportsShouldProcess=$true)]
-  param (
-    [Parameter(Mandatory=$true)]
-    [string] $Name
-  )
-  $Path = Join-Path (Get-Location) $Name
-  if (Test-Path $Path) {
-    $Cleaned = Resolve-Path $Path
-    Write-Verbose "=> Deleting $Cleaned"
-    Remove-Item -Path $Cleaned -Recurse
-    Write-Verbose "=> Deleted $Cleaned"
-  } else {
-    Write-Error 'Bad input. No folders/files were deleted'
-  }
-}
-
-function Take
-{
-  <#
-  .SYNOPSIS
-  Powershell equivalent of oh-my-zsh take function
-  .DESCRIPTION
-  Using take will create a new directory and then enter the driectory
-  .EXAMPLE
-  take <folder name>
-  #>
-  [CmdletBinding(SupportsShouldProcess=$true)]
-  param (
-    [Parameter(Mandatory=$true)]
-    [string] $Name
-  )
-  $Path = Join-Path (Get-Location) $Name
-  if (Test-Path $Path) {
-    Write-Verbose "=> $Path exists"
-    Write-Verbose "=> Entering $Path"
-    Set-Location $Path
-  } else {
-    Write-Verbose "=> Creating $Path"
-    mkdir $Path
-    if (Test-Path $Path) {
-      Write-Verbose "=> Entering $Path"
-      Set-Location $Path
-    }
-  }
-  Write-Verbose "=> pwd is $(Get-Location)"
-}
-
-function New-File
-{
-  <#
-  .SYNOPSIS
-  Powershell equivalent of linux "touch" command
-  .EXAMPLE
-  New-File <file name>
-  #>
-  [CmdletBinding(SupportsShouldProcess=$true)]
-  param (
-    [Parameter(Mandatory=$true)]
-    [string] $Name
-  )
-  if (Test-Path $Name) {
-    (Get-ChildItem $Name).LastWriteTime = Get-Date
-  } else {
-    New-Item -Path . -Name $Name -ItemType "file" -Value ""
-  }
-}
-
-function New-SshKey
-{
-  [CmdletBinding()]
-  param(
-    [Parameter()]
-    [string] $Name="id_rsa")
-  Write-Verbose "==> Generating SSH key pair"
-  $Path = "~/.ssh/$Name"
-  ssh-keygen --% -q -b 4096 -t rsa -N "" -f TEMPORARY_FILE_NAME
-  Move-Item -Path TEMPORARY_FILE_NAME -Destination $Path
-  Move-Item -Path TEMPORARY_FILE_NAME.pub -Destination "$Path.pub"
-  if (Test-Path "$Path.pub") {
-    Write-Verbose "==> $Name SSH private key saved to $Path"
-    Write-Verbose "==> Saving SSH public key to clipboard"
-    Get-Content "$Path.pub" | Set-Clipboard
-    Write-Output "==> Public key saved to clipboard"
-  } else {
-    Write-Error "==> Failed to create SSH key"
-  }
-}
-
 function Install-SshServer
 {
   <#
@@ -221,8 +176,20 @@ function Install-SshServer
   Write-Verbose '=> Adding firewall rule for sshd'
   New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
 }
-
+#
 # Aliases
+#
+function home { Set-Location ~ }
+function Invoke-DockerInspectAddress { docker inspect --format '{{ .NetworkSettings.IPAddress }}' $args[0] }
+function Invoke-DockerRemoveAll { docker stop $(docker ps -a -q); docker rm $(docker ps -a -q) }
+function Invoke-DockerRemoveAllImages { docker rmi $(docker images -a -q) }
+function Invoke-GitCommand { git $args }
+function Invoke-GitCommit { git commit -vam $args }
+function Invoke-GitDiff { git diff $args }
+function Invoke-GitPushMaster { git push origin master }
+function Invoke-GitStatus { git status -sb }
+function Invoke-GitRebase { git rebase -i $args }
+function Invoke-GitLog { git log --oneline --decorate }
 Set-Alias -Name ~ -Value home -Option AllScope
 Set-Alias -Name la -Value Get-ChildItem -Option AllScope
 Set-Alias -Name ls -Value Get-ChildItemColorFormatWide -Option AllScope
@@ -233,8 +200,39 @@ Set-Alias -Name drai -Value Invoke-DockerRemoveAllImages -Option AllScope
 Set-Alias -Name g -Value Invoke-GitCommand -Option AllScope
 Set-Alias -Name gcam -Value Invoke-GitCommit -Option AllScope
 Set-Alias -Name gd -Value Invoke-GitDiff -Option AllScope
-Set-Alias -Name gpom -Value Invoke-GitPushMaster -Option AllScope
-Set-Alias -Name gsb -Value Invoke-GitStatus -Option AllScope
 Set-Alias -Name glo -Value Invoke-GitLog -Option AllScope
+Set-Alias -Name gpom -Value Invoke-GitPushMaster -Option AllScope
 Set-Alias -Name grbi -Value Invoke-GitRebase -Option AllScope
+Set-Alias -Name gsb -Value Invoke-GitStatus -Option AllScope
 Set-Alias -Name touch -Value New-File -Option AllScope
+#
+# Install Powershell modules, if not already installed
+#
+if (Test-Installed posh-git) {
+  Import-Module posh-git
+}
+if (Test-Installed oh-my-posh) {
+  Import-Module oh-my-posh
+}
+if (Test-Installed Get-ChildItemColor) {
+  Import-Module Get-ChildItemColor
+}
+#
+# Set Oh-my-posh theme
+#
+Set-Theme Agnoster
+#
+# Import Chocolatey profile
+#
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path $ChocolateyProfile) {
+  Import-Module "$ChocolateyProfile"
+}
+#
+# Directory traversal shortcuts
+#
+for($i = 1; $i -le 5; $i++) {
+  $u =  "".PadLeft($i,"u")
+  $d =  $u.Replace("u","../")
+  Invoke-Expression "function $u { push-location $d }"
+}
--- dev-with-windows-terminal/README.md
+++ dev-with-windows-terminal/README.md
@@ -1,2 +1,70 @@
 Development with Windows Terminal
 =================================
+
+Quick Start
+-----------
+1. Install [Windows Terminal]()
+2. Open a Powershell terminal in a location, `C:/path/to/folder`, where you can save some files
+
+> While holding <key>shift</key>, right-click `C:/path/to/folder` directory and select "Open PowerShell window here"
+
+3. Clone this repository and navigate to `dev-with-windows-terminal` directory:
+
+```bash
+git clone https://github.com/jhwohlgemuth/env
+cd env/dev-with-windows-terminal
+```
+
+4. Install PowerShell modules and Chocolatey packages:
+> You are encouraged to read the content of [install.ps1](./install.ps1)
+
+```powershell
+./install.ps1
+```
+
+5. Copy content of [Microsoft.Powershell_profile.ps1](./Microsoft.Powershell_profile.ps1) into Windows Terminal settings:
+
+```powershell
+Set-Content -Path $PROFILE -Value (Get-Content -Path .\Microsoft.Powershell_profile.ps1)
+```
+
+6. Open the Windows terminal `settings.json` file by pressing <key>CTRL</key>+<key>,</key> and replace contents with content of [settings.json](./settings.json) from this repository
+
+7. Enjoy your awesome new ***Windows*** terminal. `#cantBelieveItsNotLinux`
+
+Now that you have an amazing terminal, [give Docker a try!](../dev-with-docker)
+
+
+[Microsoft.Powershell_profile.ps1](./Microsoft.Powershell_profile.ps1) provides the following functions:
+> Use `Get-Help <Function-Name>` to see usage details. **Example**: `Get-Help Find-Duplicates -examples`
+
+- `Find-Duplicates`
+- `New-File`
+- `New-SshKey`
+- `Remove-DirectoryForce`
+- `Take`
+- `Test-Admin`
+- `Test-Empty`
+- `Test-Installed`
+- `Install-ModuleMaybe`
+- `Install-SshServer`
+
+
+[Microsoft.Powershell_profile.ps1](./Microsoft.Powershell_profile.ps1) provides the following aliases:
+> Use `Get-Alias <Name>` to see alias details. **Example**: `Get-Alias dra`
+
+- `~`
+- `dip`
+- `dra`
+- `drai`
+- `g`
+- `gcam`
+- `gd`
+- `gpom`
+- `grbi`
+- `gsb`
+- `la`
+- `ls`
+- `rf`
+- `touch`
+- `u`, `uu`, `uuu`, `uuuu`, `uuuuu`
