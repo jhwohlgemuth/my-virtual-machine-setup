3244b2ba897fefeb2cdcf84af82a31c9186e7b34
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2020-09-07 16:15:01 -0500

    feat: Add clj-kondo to clj setup

--- dev-with-docker/profiles.clj
+++ dev-with-docker/profiles.clj
@@ -1,10 +1,12 @@
 {:user {
         :aliases {
                   "rebl" ["run" "-m" "rebel-readline.main"]
-                  "lint" ["do" ["cljfmt" "check"] "kibit"]
+                  "lint" ["run" "-m" "clj-kondo.main"]
+                  "fmt" ["do" ["cljfmt" "check"] "kibit"]
                   "fix" ["do" ["cljfmt" "fix"] ["kibit" "--replace" "--interactive"]]}
         :dependencies [
-                  [com.bhauman/rebel-readline "0.1.4"]]
+                  [com.bhauman/rebel-readline "0.1.4"]
+                  [clj-kondo "RELEASE"]]
         :plugins [
                   [lein-drip "0.1.1-SNAPSHOT"];; faster JVM
                   ;; Ultra is awesome, but v0.6.0 has issues with JDK v11
