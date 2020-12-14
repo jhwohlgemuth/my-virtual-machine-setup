3185c9f768feec4460adec454dd8c412625364f1
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2020-12-13 19:01:02 -0600

    feat: Add aliases for git and docker to PROFILE

--- dev-with-windows-terminal/Microsoft.Powershell_profile.ps1
+++ dev-with-windows-terminal/Microsoft.Powershell_profile.ps1
@@ -1,9 +1,8 @@
 #
-# Install Powershell modules, if not already installed
+# Import Powershell modules
 #
-function Test-Installed
-{
-  $Name = $args[0]
+function Test-Installed {
+  $Name = $Args[0]
   Get-Module -ListAvailable -Name $Name
 }
 if (Test-Installed PSReadLine) {
@@ -39,7 +38,38 @@ if (Test-Path $ChocolateyProfile) {
   Import-Module "$ChocolateyProfile"
 }
 #
-# Directory traversal shortcuts
+# Set aliases
+#
+if (Test-Installed Get-ChildItemColor) {
+  Set-Alias -Scope Global -Option AllScope -Name la -Value Get-ChildItemColor
+  Set-Alias -Scope Global -Option AllScope -Name ls -Value Get-ChildItemColorFormatWide
+}
+if (Get-Command -Name git) {
+  function Invoke-GitCommand { git $Args }
+  function Invoke-GitCommit { git commit -vam $Args }
+  function Invoke-GitDiff { git diff $Args }
+  function Invoke-GitPushMaster { git push origin master }
+  function Invoke-GitStatus { git status -sb }
+  function Invoke-GitRebase { git rebase -i $Args }
+  function Invoke-GitLog { git log --oneline --decorate }
+  Set-Alias -Scope Global -Option AllScope -Name g -Value Invoke-GitCommand
+  Set-Alias -Scope Global -Option AllScope -Name gcam -Value Invoke-GitCommit
+  Set-Alias -Scope Global -Option AllScope -Name gd -Value Invoke-GitDiff
+  Set-Alias -Scope Global -Option AllScope -Name glo -Value Invoke-GitLog
+  Set-Alias -Scope Global -Option AllScope -Name gpom -Value Invoke-GitPushMaster
+  Set-Alias -Scope Global -Option AllScope -Name grbi -Value Invoke-GitRebase
+  Set-Alias -Scope Global -Option AllScope -Name gsb -Value Invoke-GitStatus
+}
+if (Get-Command -Name docker) {
+  function Invoke-DockerInspectAddress { docker inspect --format '{{ .NetworkSettings.IPAddress }}' $Args[0] }
+  function Invoke-DockerRemoveAll { docker stop $(docker ps -a -q); docker rm $(docker ps -a -q) }
+  function Invoke-DockerRemoveAllImage { docker rmi $(docker images -a -q) }
+  Set-Alias -Scope Global -Option AllScope -Name dip -Value Invoke-DockerInspectAddress
+  Set-Alias -Scope Global -Option AllScope -Name dra -Value Invoke-DockerRemoveAll
+  Set-Alias -Scope Global -Option AllScope -Name drai -Value Invoke-DockerRemoveAllImage
+}
+#
+# Create directory traversal shortcuts
 #
 for($i = 1; $i -le 5; $i++) {
   $u =  "".PadLeft($i,"u")
