e1650c31ef7e44296e49b6eb0e596456a891d7b5
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2022-01-10 11:31:37 -0600

    feat: Add theme and intial actions

--- dev-with-windows-terminal/Invoke-Setup.ps1
+++ dev-with-windows-terminal/Invoke-Setup.ps1
@@ -7,8 +7,33 @@ Param(
     [Switch] $Initial,
     [Switch] $EnableRemoting,
     [Switch] $SkipInstall,
-    [PSObject] $InstallOptions = @{ PackageManager = 'Scoop'; Include = 'extra'}
+    [PSObject] $InstallOptions = @{ PackageManager = 'Scoop'; Include = 'extra'},
+    [ValidateSet(
+        'agnoster',
+        'aliens',
+        'blueish',
+        'iterm2',
+        'lambda',
+        'microverse',
+        'night-owl',
+        'paradox',
+        'powerlevel',
+        'princess',
+        'tonybaloney',
+        'robbyrussel',
+        'slim',
+        'space',
+        'star',
+        'wopian'
+    )]
+    [String] $Theme = 'powerline'
 )
+if ($Initial) {
+    if ($PSCmdlet.ShouldProcess('==> [INFO] Create dev folder and install git')) {
+        New-Item -ItemType Directory -Name dev -Path '~/dev/foo' -Force
+        scoop install git
+    }
+}
 if (-not $SkipInstall) {
     if ($PSCmdlet.ShouldProcess('==> [RUN] Invoke-Install.ps1')) {
         '==> [RUN] Executing Invoke-Install.ps1' | Write-Verbose
@@ -29,4 +54,28 @@ if ($PSCmdlet.ShouldProcess('==> [INFO] Copy settings JSON file')) {
     $LocalSettingsPath = "$Env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
     "==> [INFO] Copying settings.json to ${LocalSettingsPath}" | Write-Verbose
     Set-Content -Path $LocalSettingsPath -Value (Get-Content -Path .\settings.json)
+}
+# Update oh-my-posh theme
+$ThemeName = @{
+    'agnoster'    = 'agnoster'
+    'aliens'      = 'aliens'
+    'blueish'     = 'blueish'
+    'iterm2'      = 'iterm2'
+    'lambda'      = 'lambda'
+    'microverse'  = 'microverse-power'
+    'night-owl'   = 'night-owl'
+    'paradox'     = 'paradox'
+    'powerlevel'  = 'powerlevel10k_rainbow'
+    'princess'    = 'M365Princess'
+    'tonybaloney' = 'tonybaloney'
+    'robbyrussel' = 'robbyrussel'
+    'slim'        = 'slim'
+    'space'       = 'space'
+    'star'        = 'star'
+    'wopian'      = 'wopian'
+}[$Theme]
+if ($PSCmdlet.ShouldProcess("==> [INFO] Update oh-my-posh theme to ${ThemeName}")) {
+    "==> [INFO] Updating oh-my-posh theme to ${ThemeName}" | Write-Verbose
+    Set-PoshPrompt -Theme $ThemeName
+    ((Get-Content -path $PROFILE -Raw) -replace 'Set-PoshPrompt -Theme .*\r\n', "Set-PoshPrompt -Theme ${ThemeName}`n") | Set-Content -Path $PROFILE
 }
