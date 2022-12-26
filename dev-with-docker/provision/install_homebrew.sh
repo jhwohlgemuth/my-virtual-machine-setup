5fa75ab8f30366fc8168d55d17aa6ec72fa12f9a
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2022-12-26 11:03:38 -0500

    feat: Add yq to homebrew provision script

--- dev-with-docker/provision/install_homebrew.sh
+++ dev-with-docker/provision/install_homebrew.sh
@@ -4,6 +4,6 @@
 # shellcheck disable=SC2016
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "${HOME}/.zshrc"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-brew install thefuck up
+brew install thefuck up yq
 # shellcheck disable=SC2016
 echo 'eval "$(thefuck --alias oops)"' >> "${HOME}/.zshrc"
