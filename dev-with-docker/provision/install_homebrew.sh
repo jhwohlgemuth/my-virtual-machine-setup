ddec35aa79c7c0e6a9b8647748633b2b8db081a6
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2022-12-26 19:06:11 -0500

    feat: Combine some layers in dockerfile; fix brew install script

--- dev-with-docker/provision/install_homebrew.sh
+++ dev-with-docker/provision/install_homebrew.sh
@@ -5,13 +5,13 @@
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "${HOME}/.zshrc"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install \
-    && broot \
-    && dust \
-    && grex \
-    && htmlq \
-    && thefuck \
-    && tokei \
-    && up \
-    && yq
+    broot \
+    dust \
+    grex \
+    htmlq \
+    thefuck \
+    tokei \
+    up \
+    yq
 # shellcheck disable=SC2016
 echo 'eval "$(thefuck --alias oops)"' >> "${HOME}/.zshrc"
