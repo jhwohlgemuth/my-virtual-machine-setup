0a1d9decee08d2251e6bcbe3ae1b2f72bd5b50b8
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2023-05-13 21:26:00 -0400

    feat: Update exposed ports

--- dev-with-docker/jupyter/jupyter_notebook_config.py
+++ dev-with-docker/jupyter/jupyter_notebook_config.py
@@ -1,5 +1,5 @@
 c.NotebookApp.ip = '*'
-c.NotebookApp.port = 4669
+c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
 c.NotebookApp.password = u'argon2:$argon2id$v=19$m=10240,t=10,p=8$bY5rSBCPqjXzzLckWsGTLg$vYg9lyo18FG1kskOrT6ShA'
 c.NotebookApp.keyfile = u"/root/certs/my.key"
