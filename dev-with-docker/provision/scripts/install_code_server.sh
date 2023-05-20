f413f4badfc5dc70d0abad2a54abb73006a8ac99
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2023-05-19 20:22:25 -0400

    feat: Move code-server installation into script

---
+++ dev-with-docker/provision/scripts/install_code_server.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#
+# Install Code Server
+#
+CODE_RELEASE=$(curl -sX GET https://api.github.com/repos/coder/code-server/releases/latest | awk '/tag_name/{print $4;exit}' FS='[""]' | sed 's|^v||')
+mkdir -p "${CODE_SERVER_CONFIG_DIR}"/{data,extensions,workspace}
+curl -o /tmp/code-server.tar.gz -L "https://github.com/coder/code-server/releases/download/v${CODE_RELEASE}/code-server-${CODE_RELEASE}-linux-amd64.tar.gz"
+tar xf /tmp/code-server.tar.gz -C /app/code-server --strip-components=1
+#
+# Install Code Server extensions
+#
+EXTENSIONS="\
+    akamud.vscode-theme-onedark \
+    emmanuelbeziat.vscode-great-icons \
+    ms-vscode.atom-keybindings \
+    ms-toolsai.jupyter \
+    ms-python.python \
+    eamodio.gitlens \
+    codeium.codeium"
+
+for EXTENSION in ${EXTENSIONS}; do
+    code-server --extensions-dir "${CODE_SERVER_CONFIG_DIR}/extensions" --install-extension "${EXTENSION}"
+done
