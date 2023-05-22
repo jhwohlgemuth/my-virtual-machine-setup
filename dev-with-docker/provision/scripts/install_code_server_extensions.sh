e52ec0c02dd6aa85b22f1d6b6cf8a4d1aa782bd4
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2023-05-21 20:45:14 -0400

    feat: Use script for installing extensions after container creation

--- dev-with-docker/provision/scripts/install_code_server_extensions.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-#
-# Install Code Server extensions
-#
-TYPE=${NOTEBOOK_ENVIRONMENT:-notebook}
-EXTENSIONS="$(jq -r ".${TYPE}[]" ./Extensions.json)"
-for EXTENSION in ${EXTENSIONS}; do
-    code-server --extensions-dir "${CODE_SERVER_CONFIG_DIR}/extensions" --install-extension "${EXTENSION}"
-done
