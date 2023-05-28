e5ed3aa92198488b237c59f3384b6f0cd0f6177d
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2023-05-28 19:29:01 -0400

    ci: Enable all github workflows

--- .github/actions/build-and-push-image/entrypoint.sh
+++ .github/actions/build-and-push-image/entrypoint.sh
@@ -1,5 +1,21 @@
-#!/bin/sh -l
+#! /bin/bash
 
 cd dev-with-containers
-make format
-echo "docker push ${REGISTRY}/${GITHUB_ACTOR}/${IMAGE_NAME}"
+echo "Build and publish:"
+if [[ ${IMAGE_NAME} == "base" ]]
+then
+    figlet "ALL"
+    make all publish
+elif [[ ${IMAGE_NAME} == "notebook" ]]
+then
+    figlet "MULITPLE"
+    make notebook python rust jvm dotnet lambda
+elif [[ ${IMAGE_NAME} == "dotnet" ]]
+then
+    figlet "dotnet & lambda"
+    make dotnet lambda
+else
+    figlet ${IMAGE_NAME}
+    make ${IMAGE_NAME}
+    docker push "${REGISTRY}/${GITHUB_ACTOR}/${IMAGE_NAME}"
+fi
