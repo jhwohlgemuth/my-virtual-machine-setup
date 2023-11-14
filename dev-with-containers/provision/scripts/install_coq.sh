aa24e79bd6219df47ec5f1fc75304ff59c6a8843
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2023-11-14 18:29:03 -0500

    feat: Clear shellcheck issues, add why3

--- dev-with-containers/provision/scripts/install_coq.sh
+++ dev-with-containers/provision/scripts/install_coq.sh
@@ -5,13 +5,24 @@
 opam repo add coq-released https://coq.inria.fr/opam/released
 opam repo add coq-core-dev https://coq.inria.fr/opam/core-dev
 opam repo add coq-extra-dev https://coq.inria.fr/opam/extra-dev
-opam pin add coq ${COQ_VERSION}
-opam install coq-hammer vscoq-language-server --yes
+opam pin add coq "${COQ_VERSION:-8.18.0}"
+opam install --yes \
+    coq-hammer \
+    vscoq-language-server
 #
 # Install Jupyter kernel
 #
-/opt/conda/bin/mamba create --name coq --channel conda-forge --yes coq-jupyter=1.6.0 python=3.10
-/opt/conda/bin/mamba clean -yaf
+if type /opt/conda/bin/mamba >/dev/null 2>&1; then
+    /opt/conda/bin/mamba create \
+        --name coq \
+        --channel conda-forge \
+        --yes \
+        python=3.10 \
+        coq-jupyter=1.6.0
+    /opt/conda/bin/mamba clean -yaf
+else
+    echo "==> [WARN] mamba is NOT installed"
+fi
 #
 # Install Aeneas, Creusot, and coq-of-rust 
 #
--- dev-with-containers/provision/scripts/install_ocaml.sh
+++ dev-with-containers/provision/scripts/install_ocaml.sh
@@ -2,18 +2,24 @@
 
 apt-get update
 apt-get install --no-install-recommends -y opam ruby-full
-opam init --compiler=${OCAML_VERSION} --disable-sandboxing --yes
-eval $(opam env)
-echo 'eval $(opam env)' >> "${HOME}/.zshrc"
-opam install dune ocaml-lsp-server odoc ocamlformat utop --yes
-eval $(opam env)
+opam init --compiler="${OCAML_VERSION:-4.14.1}" --disable-sandboxing --yes
+eval "$(opam env)"
+# shellcheck disable=SC2016
+echo 'eval "$(opam env)"' >> "${HOME}/.zshrc"
+opam --yes install \
+    dune \
+    ocaml-lsp-server \
+    odoc \
+    ocamlformat \
+    utop
+eval "$(opam env)"
 opam upgrade
-eval $(opam env)
+eval "$(opam env)"
 #
 # Install Jupyter kernel
 #
-opam install jupyter --yes
-eval $(opam env)
+opam install --yes jupyter
+eval "$(opam env)"
 ocaml-jupyter-opam-genspec
 jupyter kernelspec install --name "OCaml" "$(opam var share)/jupyter"
 sed -i 's/OCaml default/OCaml/' /usr/local/share/jupyter/kernels/ocaml/kernel.json
