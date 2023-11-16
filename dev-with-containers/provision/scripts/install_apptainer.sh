bafaeb34be37e2ff079b8dc9172f644b2f33b068
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2023-11-16 07:37:57 -0500

    fix: Re-add update for apptainer install; add necessary flags for curl in conda install script

--- dev-with-containers/provision/scripts/install_apptainer.sh
+++ dev-with-containers/provision/scripts/install_apptainer.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
 curl -LOJ https://github.com/apptainer/apptainer/releases/download/v1.2.4/apptainer_1.2.4_amd64.deb
-apt install -y ./apptainer_1.2.4_amd64.deb
+apt-get update
+apt-get install -y ./apptainer_1.2.4_amd64.deb
 rm ./apptainer_1.2.4_amd64.deb
--- dev-with-containers/provision/scripts/install_conda.sh
+++ dev-with-containers/provision/scripts/install_conda.sh
@@ -16,6 +16,6 @@ zsh -c "/root/miniconda3/bin/conda init powershell"
 TEMP_SCRIPT_PATH=/tmp/miniforge.sh
 MINIFORGE_NAME=Miniforge3
 MINIFORGE_VERSION=23.3.1-1
-curl -o "${TEMP_SCRIPT_PATH}" -s "https://github.com/conda-forge/miniforge/releases/download/${MINIFORGE_VERSION}/${MINIFORGE_NAME}-${MINIFORGE_VERSION}-Linux-$(uname -m).sh"
+curl -o "${TEMP_SCRIPT_PATH}" -LJ "https://github.com/conda-forge/miniforge/releases/download/${MINIFORGE_VERSION}/${MINIFORGE_NAME}-${MINIFORGE_VERSION}-Linux-$(uname -m).sh"
 /bin/bash "${TEMP_SCRIPT_PATH}" -b -p "${CONDA_DIR}"
 rm "${TEMP_SCRIPT_PATH}"
