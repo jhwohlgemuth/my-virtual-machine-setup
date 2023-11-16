60b5b82205fe923a0b58213bbe0b6ce6b02396fb
Jason Wohlgemuth <jhwohlgemuth@users.noreply.github.com>
2023-11-15 21:41:00 -0500

    Add necessary curl flags

--- dev-with-containers/provision/scripts/install_apptainer.sh
+++ dev-with-containers/provision/scripts/install_apptainer.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-curl -O https://github.com/apptainer/apptainer/releases/download/v1.2.4/apptainer_1.2.4_amd64.deb
+curl -LOJ https://github.com/apptainer/apptainer/releases/download/v1.2.4/apptainer_1.2.4_amd64.deb
 apt install -y ./apptainer_1.2.4_amd64.deb
 rm ./apptainer_1.2.4_amd64.deb
