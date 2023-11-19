1595c59a4f23d4d53d27d9aad482df79ba0f97a4
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2023-11-18 19:58:44 -0500

    feat: Switch from notebook to lab

--- dev-with-containers/jupyter/jupyter_notebook_config.py
+++ dev-with-containers/jupyter/jupyter_notebook_config.py
@@ -1,3 +1,6 @@
+#
+# Jupyter Notebook
+#
 c.NotebookApp.ip = '*'
 c.NotebookApp.port = 13337
 c.NotebookApp.open_browser = False
@@ -5,4 +8,13 @@ c.NotebookApp.password = u'argon2:$argon2id$v=19$m=10240,t=10,p=8$bY5rSBCPqjXzzL
 c.NotebookApp.keyfile = u"/root/certs/my.key"
 c.NotebookApp.certfile = u"/root/certs/my.pem"
 c.NotebookApp.notebook_dir = "/root/dev/notebooks"
-c.Session.debug = True
+#
+# Jupyter Lab
+#
+c.ServerApp.ip = '*'
+c.ServerApp.port = 13337
+c.ServerApp.open_browser = False
+c.ServerApp.password = u'argon2:$argon2id$v=19$m=10240,t=10,p=8$bY5rSBCPqjXzzLckWsGTLg$vYg9lyo18FG1kskOrT6ShA'
+c.ServerApp.keyfile = u"/root/certs/my.key"
+c.ServerApp.certfile = u"/root/certs/my.pem"
+c.ServerApp.root_dir = "/root/dev/notebooks"
