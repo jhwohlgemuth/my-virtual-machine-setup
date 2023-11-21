7fa9bd3a441edd2ee61c3af556d3761c08c6c6fe
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2023-11-20 20:02:12 -0500

    feat: Clear lint issues; cull python extras

--- dev-with-containers/provision/scripts/common.sh
+++ dev-with-containers/provision/scripts/common.sh
@@ -10,10 +10,18 @@ function requires {
         fi
     done
 }
+
 function install_dotnet_kernel {
     requires dotnet jupyter zsh
     dotnet tool install --global Microsoft.dotnet-interactive
     dotnet interactive jupyter install
     # shellcheck disable=SC2016
     echo 'export PATH="${PATH}:/root/.dotnet/tools"' >> "${HOME}/.zshrc"
+}
+
+function install_codon {
+    requires curl python zsh
+    /bin/bash -c "$(curl -fsSL https://exaloop.io/install.sh)"
+    # shellcheck disable=SC2016
+    echo 'export PATH="${HOME}/.codon/bin:${PATH}"' >> "${HOME}/.zshrc"
 }
