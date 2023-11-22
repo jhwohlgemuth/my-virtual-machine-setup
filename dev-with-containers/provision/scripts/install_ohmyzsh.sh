97630a22888db3b436329cd4ab66b8e04b3021ac
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2023-11-21 19:54:26 -0500

    feat: Clean up alias organization a bit in zshrc

--- dev-with-containers/provision/scripts/install_ohmyzsh.sh
+++ dev-with-containers/provision/scripts/install_ohmyzsh.sh
@@ -13,17 +13,16 @@ sh -c "$(curl https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-
     -p https://github.com/zsh-users/zsh-syntax-highlighting \
     -p https://github.com/conda-incubator/conda-zsh-completion \
     -p https://github.com/jhwohlgemuth/zsh-handy-helpers \
-    -p https://github.com/jhwohlgemuth/zsh-pentest \
-    -a 'bindkey "\$terminfo[kcuu1]" history-substring-search-up' \
-    -a 'bindkey "\$terminfo[kcud1]" history-substring-search-down' \
-    -a '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh'
+    -p https://github.com/jhwohlgemuth/zsh-pentest
 #
 # Customize .zshrc
 #
 sed -i "s/export TERM=xterm/export TERM=xterm-256color/g" "${HOME}/.zshrc"
 {
-    echo "ZLE_RPROMPT_INDENT=0"
-    echo "alias python=python3"
-    echo "alias pip=pip3"
+    echo 'bindkey "\$terminfo[kcuu1]" history-substring-search-up'
+    echo 'bindkey "\$terminfo[kcud1]" history-substring-search-down'
+    echo 'ZLE_RPROMPT_INDENT=0'
+    echo 'alias python=python3'
+    echo 'alias pip=pip3'
     echo "source ${HOME}/.p10k.zsh"
 } >> "${HOME}/.zshrc"
