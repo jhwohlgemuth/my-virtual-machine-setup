cc86b552ba27103fa5cff685a0a72c3853c5b681
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2023-12-02 20:32:39 -0500

    feat: Move ohmyzsh configuration to separate file

---
+++ dev-with-containers/provision/scripts/dev/configure_ohmyzsh.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+set -e
+
+requires \
+    ast-grep \
+    direnv \
+    fuck \
+    locales \
+    python3
+main() {
+    #
+    # Customize .zshrc
+    #
+    locale-gen
+    sed -i "/export LC_ALL=/d" "${HOME}/.zshrc"
+    sed -i "/export LANG=/d" "${HOME}/.zshrc"
+    sed -i "/export LANGUAGE=/d" "${HOME}/.zshrc"
+    sed -i "s/export TERM=xterm/export TERM=xterm-256color/g" "${HOME}/.zshrc"
+    # shellcheck disable=SC2016
+    {
+        echo 'bindkey "\$terminfo[kcuu1]" history-substring-search-up'
+        echo 'bindkey "\$terminfo[kcud1]" history-substring-search-down'
+        echo 'ZLE_RPROMPT_INDENT=0'
+        echo 'eval "$(direnv hook zsh)"' >> "${HOME}/.zshrc"
+        echo 'eval "$(thefuck --alias oops)"' >> "${HOME}/.zshrc"
+        echo "source ${HOME}/.p10k.zsh"
+        echo 'alias python=python3'
+        echo 'alias pip=pip3'
+        echo 'alias sgrep=ast-grep' >> "${HOME}/.zshrc"
+    } >> "${HOME}/.zshrc"
+}
+main "$@"
--- dev-with-containers/provision/scripts/dev/install_ohmyzsh.sh
+++ dev-with-containers/provision/scripts/dev/install_ohmyzsh.sh
@@ -18,21 +18,5 @@ main() {
         -p https://github.com/conda-incubator/conda-zsh-completion \
         -p https://github.com/jhwohlgemuth/zsh-handy-helpers \
         -p https://github.com/jhwohlgemuth/zsh-pentest
-    #
-    # Customize .zshrc
-    #
-    sed -i "/export LC_ALL=/d" "${HOME}/.zshrc"
-    sed -i "/export LANG=/d" "${HOME}/.zshrc"
-    sed -i "/export LANGUAGE=/d" "${HOME}/.zshrc"
-    sed -i "s/export TERM=xterm/export TERM=xterm-256color/g" "${HOME}/.zshrc"
-    # shellcheck disable=SC2016
-    {
-        echo 'bindkey "\$terminfo[kcuu1]" history-substring-search-up'
-        echo 'bindkey "\$terminfo[kcud1]" history-substring-search-down'
-        echo 'ZLE_RPROMPT_INDENT=0'
-        echo 'alias python=python3'
-        echo 'alias pip=pip3'
-        echo "source ${HOME}/.p10k.zsh"
-    } >> "${HOME}/.zshrc"
 }
 main "$@"
