4c540c802a551dc4dbffbc4478e9b2e97da8ef56
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2024-01-09 15:59:16 -0500

    feat: Move install_extensions into notebook scripts

---
+++ dev-with-containers/provision/scripts/notebook/install_extensions.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+set -e
+
+requires \
+    code-server \
+    curl \
+    jq
+main() {
+    #
+    # Download Extensions listing
+    #
+    EXTENSIONS_JSON="${CODE_SERVER_CONFIG_DIR}"/Extensions.json
+    curl -o "${EXTENSIONS_JSON}" https://raw.githubusercontent.com/jhwohlgemuth/env/master/dev-with-containers/config/code-server/Extensions.json
+    #
+    # Install Code Server extensions
+    #
+    if [ -z "$1" ]
+    then
+        ARGS=notebook
+    else
+        ARGS=("$@")
+    fi
+    for ARG in "${ARGS[@]}"; do
+        echo "Installing extensions for ${ARG} VS Code instance"
+        EXTENSIONS="$(jq -r ".${ARG}[]" "${EXTENSIONS_JSON}")"
+        for EXTENSION in ${EXTENSIONS}; do
+            code-server --extensions-dir "${CODE_SERVER_CONFIG_DIR}/extensions" --install-extension "${EXTENSION}" --force
+        done
+    done
+}
+main "$@"
